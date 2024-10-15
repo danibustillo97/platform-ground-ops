9653337fec15afdc7231e98e8bc51352908b69b8
Danilo Bustillo <danilo.bustillo@co.qfdsolutions.com>
2024-10-15 16:47:54 -0500

    Update 1510202402 'Danilo Bustillo'

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,18 @@
-export { default } from "next-auth/middleware";
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+    const token = await getToken({ req });
+    
+    if (!token) {
+        // Redirigir a la página de inicio de sesión si no hay token
+        return NextResponse.redirect(new URL("/api/auth/signin", req.url));
+    }
+
+    // Continuar con la solicitud
+    return NextResponse.next();
+}
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+    matcher: ["/dashboard/:path*"],
 };
